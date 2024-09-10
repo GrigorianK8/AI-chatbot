de40f511974b98cbe633c26816bcc4b21d7ff1e5
GrigorianK8 <karogrigorian8@gmail.com>
2024-09-10 19:17:26 +0400

    backend(fixed mistake)

--- chatbot-backend/src/main/java/com/grigoriank/chatbotBackend/controller/ChatbotController.java
+++ chatbot-backend/src/main/java/com/grigoriank/chatbotBackend/controller/ChatbotController.java
@@ -6,13 +6,11 @@ import com.grigoriank.chatbotBackend.service.ChatbotService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin
 @RequestMapping("/ai/chat")
 public class ChatbotController {
 
